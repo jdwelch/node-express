d9de8f686ce7a596a512aa473772ef1cdb0b911d
JD Welch <jdwelch@puppet.com>
2018-02-23 16:52:48 +0000

    handlebars!

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
+var os = require('os')
+
+app.set('view engine', 'hbs');
+
 app.get('/', function (req, res) {
-  res.send('<h1 style="background-color:#55B4B0;font-family:Helvetica Neue;color:#9B2335">Hello, please deploy this change!</h1>');
+  res.render('index', { hostname: os.hostname() } );
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
