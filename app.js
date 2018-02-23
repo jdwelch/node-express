3976b4b916f8684bb47fe904af970cf20ed7ba44
JD Welch <jdwelch@puppet.com>
2018-02-23 15:26:47 +0000

    is success repeatable?

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('<h1 style="font-family:Helvetica Neue">Hello, please deploy this change!</h1>');
+  res.send('<h1 style="background-color:#55B4B0;font-family:Helvetica Neue;color:#9B2335">Hello, please deploy this change!</h1>');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
