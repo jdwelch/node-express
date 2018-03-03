92aea7a1e91fcc4327452d57b17c55cca13c5d9c
JD Welch <jdwelch@puppet.com>
2018-03-03 15:57:16 +0000

    prettier (?)

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const os = require('os')
-const bodyParser= require('body-parser')
-const mongoose = require('mongoose');
+const express = require("express");
+const os = require("os");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 // APP
 var app = express();
@@ -11,105 +11,121 @@ app.use(bodyParser.json());
 var msgSchema = new mongoose.Schema({
   text: String,
   created: Date,
-  updated: Date,
-})
-var Message = mongoose.model('Message', msgSchema)
+  updated: Date
+});
+var Message = mongoose.model("Message", msgSchema);
 
 // DATABASE
 var db = mongoose.connection;
-mongoose.connect('mongodb://localhost:27017/node-express-db');
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+mongoose.connect("mongodb://localhost:27017/node-express-db");
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
   app.listen(80, () => {
-    console.log('App listening on port 80!');
-  })
-  console.log("Connected to MongoDB!")
+    console.log("App listening on port 80!");
+  });
+  console.log("Connected to MongoDB!");
 });
 
 // GUI
-app.set('view engine', 'hbs');
-app.get('/', (req, res) => {
-  Message.find({}).exec(function (err, messages) {
+app.set("view engine", "hbs");
+app.get("/", (req, res) => {
+  Message.find({}).exec(function(err, messages) {
     if (err) {
       console.log("Error:", err);
+    } else {
+      res.render("index", { hostname: os.hostname(), messages: messages });
     }
-    else {
-      res.render('index', { hostname: os.hostname(), messages: messages } );
-    }
-  })
-})
+  });
+});
 
 // API
 
-app.get('/message', (req, res) => {
-  Message.find (function (err, findMessages) {
-    if (err) { 
-      console.log(err)
-      res.status(500).send({message: "Some error occurred while retrieving notes."});
-    } else {
-      res.send(findMessages)
-    }
-  })
-})
-
-app.post('/message', (req, res) => {
-  if(!req.body.text) {
-    return res.status(400).send({message: "Your message can not be empty"});
-  }
-  var createdTime = new Date()
-  var newMessage = new Message({ text:req.body.text, created:createdTime })
-  newMessage.save( (err, data) => {
-    if(err) {
+app.get("/message", (req, res) => {
+  Message.find(function(err, findMessages) {
+    if (err) {
       console.log(err);
-      res.status(500).send({message: "Some error occurred while creating the Note."});
+      res
+        .status(500)
+        .send({ message: "Some error occurred while retrieving notes." });
+    } else {
+      res.send(findMessages);
+    }
+  });
+});
+
+app.post("/message", (req, res) => {
+  if (!req.body.text) {
+    return res.status(400).send({ message: "Your message can not be empty" });
+  }
+  var createdTime = new Date();
+  var newMessage = new Message({ text: req.body.text, created: createdTime });
+  newMessage.save((err, data) => {
+    if (err) {
+      console.log(err);
+      res
+        .status(500)
+        .send({ message: "Some error occurred while creating the Note." });
     } else {
       res.send(data);
     }
-  })
-})
+  });
+});
 
-app.put('/message', (req, res) => {
-  var msgId = req.query.id
+app.put("/message", (req, res) => {
+  var msgId = req.query.id;
   Message.findById(msgId, (err, updateMessage) => {
-    if(err) {
+    if (err) {
       console.log(err);
-      if(err.kind === 'ObjectId') {
-        return res.status(404).send({message: "Note not found with id " + msgId});                
+      if (err.kind === "ObjectId") {
+        return res
+          .status(404)
+          .send({ message: "Note not found with id " + msgId });
       }
-      return res.status(500).send({message: "Error finding note with id " + msgId});
+      return res
+        .status(500)
+        .send({ message: "Error finding note with id " + msgId });
     }
 
-    if(!message) {
-      return res.status(404).send({message: "Note not found with id " + msgId});            
+    if (!updateMessage) {
+      return res
+        .status(404)
+        .send({ message: "Note not found with id " + msgId });
     }
 
     updateMessage.text = req.body.text;
-    updateMessage.updated = new Date()
-    
-    updateMessage.save( (err, data) => {
-      if(err) {
-        res.status(500).send({message: "Could not update note with id " + msgId});
+    updateMessage.updated = new Date();
+
+    updateMessage.save((err, data) => {
+      if (err) {
+        res
+          .status(500)
+          .send({ message: "Could not update note with id " + msgId });
       } else {
         res.send(data);
       }
     });
+  });
+});
 
-  })
-})
-
-app.delete('/message', (req,res) => {
-  var msgId = req.query.id
+app.delete("/message", (req, res) => {
+  var msgId = req.query.id;
   Message.findByIdAndRemove(msgId, (err, deleteMessage) => {
-    if(err) {
+    if (err) {
       console.log(err);
-      if(err.kind === 'ObjectId') {
-        return res.status(404).send({message: "Note not found with id " + msgId});
+      if (err.kind === "ObjectId") {
+        return res
+          .status(404)
+          .send({ message: "Note not found with id " + msgId });
       }
-      return res.status(500).send({message: "Could not delete note with id " + msgId});
+      return res
+        .status(500)
+        .send({ message: "Could not delete note with id " + msgId });
     }
-    if(!deleteMessage) {
-      return res.status(404).send({message: "Note not found with id " + msgId});
+    if (!deleteMessage) {
+      return res
+        .status(404)
+        .send({ message: "Note not found with id " + msgId });
     }
-    res.send({message: "Note "+ msgId + "deleted successfully!"})
-  })
-})
+    res.send({ message: "Note " + msgId + "deleted successfully!" });
+  });
+});
