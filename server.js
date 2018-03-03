efde1d8464ccace36372bd9f1625b7ebf15b47c0
JD Welch <jdwelch@puppet.com>
2018-03-03 12:52:04 +0000

    all json all the time

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser= require('body-parser')
 var os = require('os')
 
 app.set('view engine', 'hbs');
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.render('index', { hostname: os.hostname() } );
@@ -12,19 +12,20 @@ app.get('/', (req, res) => {
 
 app.get('/message', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.send('{"message":"foo"}')
+  res.send('{"message":"TODO FIXME"}')
 })
 
 app.post('/message', (req, res) => {
   // db.collection('messages').save(req.body, (err, result) => {
   //   if (err) return console.log(err)
-
-  //   console.log('saved to database')
   // })
-  console.log(req.query)
+  console.log(req.body)
+  // console.log('saved to database')
+  res.setHeader('Content-Type', 'application/json');
+  res.write(JSON.stringify(req.body))
   res.send()
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(80, function () {
+  console.log('"node-express" listening on port 80!');
 });
